fec685942fa47436ccdefeb9808d81125158f690
Nilson Ivano <nilson.ivano@gmail.com>
2015-11-27 09:28:07 -0200

    Commit

--- both/router/routes.js
+++ both/router/routes.js
@@ -1,9 +1,26 @@
 Router.route('/', {
-  name: 'home'
+  name: 'landing',
+  controller: PreloadController,
+  'preload':{
+      'verbose':true,
+      'styles':['styles/flat-ui/css/flat-ui.css',
+          'styles/common-files/css/icon-font.css',
+          'styles/common-files/css/animations.css',
+          'styles/ui-kit/ui-kit-contacts/css/style.css',
+          'styles/ui-kit/ui-kit-footer/css/style.css',
+          'styles/ui-kit/ui-kit-content/css/style.css',
+          'styles/ui-kit/ui-kit-header/css/style.css'],
+      'sync':['plugins/easing.min.js',
+          'plugins/jquery.svg.js',
+          'plugins/jquery.svganim.js',
+          'plugins/modernizr.custom.js',
+          'plugins/page-transitions.js',
+          'plugins/startup-kit.js']
+  }
 });
 
 Router.route('/login', {
-  name: 'login'
+    name: 'login'
 });
 
 Router.route('/register', {
@@ -17,7 +34,18 @@ Router.route('/truckInformation', {
 
 Router.route('/truckAgenda', {
     name: 'truckAgenda',
-    layoutTemplate: 'appLayout'
+    layoutTemplate: 'appLayout',
+    controller: PreloadController,
+    'preload':{
+        'verbose':true,
+        'styles':['styles/06_dataTables.bootstrap.css',
+            'styles/07_daterangepicker-bs3.css',
+            'styles/08_jquery.dataTables.min.css'],
+        'sync':['plugins/02_jquery.dataTables.min.js',
+            'plugins/03_dataTables.bootstrap.min.js',
+            'plugins/04_daterangepicker.js',
+            'plugins/05_moment.min.js']
+    }
 });
 
 Router.route('/truckImages', {
--- client/templates/truckAgenda/truckAgenda.js
+++ client/templates/truckAgenda/truckAgenda.js
@@ -12,15 +12,61 @@ Template.truckAgenda.onRendered(function(){
     $("#example1").DataTable();
 
     //GoogleMaps Autocomplete
-    var defaultBounds = new google.maps.LatLngBounds(
-        new google.maps.LatLng(5.775283, -74.344883),
-        new google.maps.LatLng(-35.590799, -35.069929));
+    //var defaultBounds = new google.maps.LatLngBounds(
+    //    new google.maps.LatLng(5.775283, -74.344883),
+    //    new google.maps.LatLng(-35.590799, -35.069929));
+    //
+    //var input = document.getElementById('agendaAddress');
+    //var options = {
+    //    bounds: defaultBounds
+    //};
+    //autocomplete = new google.maps.places.Autocomplete(input, options);
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+    //var
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
 
-    var input = document.getElementById('agendaAddress');
-    var options = {
-        bounds: defaultBounds
-    };
-    autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    //Colocando o Marker
+    var map = GoogleMaps.maps.mapAgenda.instance;
+    var user = Meteor.userId();
+    var agendas = truckAgenda.find({addedBy:user}).fetch();
+    var agendaLength = agendas.length;
+    var markerImage = '/images/markerFt.png';
+    for(i=0; i<agendaLength; i++){
+        latNumber = Number(agendas[i].lat);
+        lngNumber = Number(agendas[i].lng);
+        agendaDate = agendas[i].dateStart.toUTCString();
+        var LatLng = {lat: latNumber, lng: lngNumber};
+        var marker = new google.maps.Marker({
+            map: map,
+            position: LatLng,
+            icon: markerImage
+        });
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
 });
 
 Template.truckAgenda.events({
@@ -40,31 +86,32 @@ Template.truckAgenda.events({
            if (status == google.maps.GeocoderStatus.OK) {
                var lat = results[0].geometry.location.lat();
                var lng = results[0].geometry.location.lng();
+               truckAgenda.insert({
+                   dateStart: dateStart,
+                   dateEnd: dateEnd,
+                   address: agendaAddress,
+                   lat: lat,
+                   lng: lng,
+                   addressReference: agendaAddressReference,
+                   //truckId: truckId,
+                   addedBy: userId
+               },function(err){
+                   if (err){
+                       console.log(err);
+                   }else{
+                       console.log("Sucesso!");
+                       $('#agendaAddress').val("");
+                       $('#agendaAddressReference').val("");
+                       $('#agendaDate').val("");
+                       toastr.success("Agenda inserida com sucesso");
+                   }
+               });
            } else {
-               alert("Geocode was not successful for the following reason: " + status);
+               alert("Endereço não encontrado: " + status);
            }
        });
 
-       truckAgenda.insert({
-           dateStart: dateStart,
-           dateEnd: dateEnd,
-           address: agendaAddress,
-           //lat: lat,
-           //lng: lng,
-           addressReference: agendaAddressReference,
-           //truckId: truckId,
-           addedBy: userId
-       },function(err){
-           if (err){
-               console.log(err);
-           }else{
-               console.log("Sucesso!");
-               $('#agendaAddress').val("");
-               $('#agendaAddressReference').val("");
-               $('#agendaDate').val("");
-               toastr.success("Agenda inserida com sucesso");
-           }
-       });
+
    },
    'change #agendaAddress':function(event){
        var agendaAddress = $('#agendaAddress').val();
@@ -73,13 +120,15 @@ Template.truckAgenda.events({
        geocoder = new google.maps.Geocoder();
        var address = agendaAddress;
        var map = GoogleMaps.maps.map.instance;
+       var markerImage = '/images/markerFt.png';
        geocoder.geocode( { 'address': address}, function(results, status) {
            if (status == google.maps.GeocoderStatus.OK) {
                console.log(results);
                map.setCenter(results[0].geometry.location);
                var marker = new google.maps.Marker({
                    map: map,
-                   position: results[0].geometry.location
+                   position: results[0].geometry.location,
+                   icon: markerImage
                });
            } else {
                toastr.error("Nenhum endereço localizado. Inserir um endereço válido");
@@ -105,6 +154,26 @@ Template.truckAgenda.helpers({
                 center: new google.maps.LatLng(-37.8136, 144.9631),
                 zoom: 16
             };
+        }
+    },
+    mapOptionsAgenda: function() {
+        if (GoogleMaps.loaded()) {
+            var user = Meteor.userId();
+            var results = truckAgenda.find({addedBy: user}).fetch();
+            if(results.length>0){
+                lat = results[0].lat;
+                lng = results[0].lng;
+                console.log(results,lat,lng);
+                return {
+                    center: new google.maps.LatLng(lat,lng),
+                    zoom: 12
+                }
+            } else{
+                return {
+                    center: new google.maps.LatLng(-37.8136, 144.9631),
+                    zoom: 4
+                };
+            }
         }
     },
     agenda: function(){
