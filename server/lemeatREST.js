66b32542f355b45d3811eea0797484989f230706
Nilson Ivano <nilson.ivano@gmail.com>
2016-09-14 07:44:31 -0300

    Adaptações REST

--- server/lemeatREST.js
+++ server/lemeatREST.js
@@ -30,11 +30,14 @@ Api.addRoute('truckList', {
             var userLat = query.lat;
             var userLng = query.lng;
             var currentTime = new Date();
-            var results = Meteor.users.find({},{fields: {'profile': 1}}).fetch();
+            var results = Meteor.users.find({}, {fields: {'profile': 1}}).fetch();
             var searchResultsDb = new Mongo.Collection(null);
-            for(i in results){
-                var agenda = truckAgenda.find({addedBy: results[i]._id, dateEnd: {$gte: currentTime}}, {sort: {dateStart: 1}}).fetch();
-                if(agenda.length > 0){
+            for (i in results) {
+                var agenda = truckAgenda.find({
+                    addedBy: results[i]._id,
+                    dateEnd: {$gte: currentTime}
+                }, {sort: {dateStart: 1}}).fetch();
+                if (agenda.length > 0) {
                     var agendaLat = agenda[0].lat;
                     var agendaLng = agenda[0].lng;
                     var distance = getDistanceFromLatLonInKm(userLat, userLng, agendaLat, agendaLng);
@@ -43,20 +46,20 @@ Api.addRoute('truckList', {
                     results[i].dateStart = agenda[0].dateStart;
                     results[i].dateEnd = agenda[0].dateEnd;
                     var dateStart = new Date(results[i].dateStart);
-                    dateStart.setHours(0,0,0,0);
+                    dateStart.setHours(0, 0, 0, 0);
                     results[i].day = dateStart;
-                    if(currentTime >= results[i].dateStart && currentTime <= results[i].dateEnd){
+                    if (currentTime >= results[i].dateStart && currentTime <= results[i].dateEnd) {
                         results[i].statusOpen = true
                     } else {
                         results[i].statusOpen = false
                     }
                     searchResultsDb.insert(results[i]);
-                } else{
+                } else {
                     results[i].haveAgenda = false;
                     searchResultsDb.insert(results[i]);
                 }
             }
-            var search = searchResultsDb.find({},{sort: {haveAgenda: -1, day:1, userDistance: 1, name: 1}}).fetch()
+            var search = searchResultsDb.find({}, {sort: {haveAgenda: -1, day: 1, userDistance: 1, name: 1}}).fetch()
             for (i in search) {
                 search[i].profile.img = "http://localhost:3000" + search[i].profile.img;
             }
@@ -65,5 +68,15 @@ Api.addRoute('truckList', {
     }
 });
 
-//Truck detailed information /truck/{id}?lat={lat}&lng={lng}
-
+//Truck detailed information /truck/{id}
+Api.addRoute('truckInfo', {
+    get: {
+        authRequired: false,
+        action: function () {
+            var query = this.queryParams;
+            var id = query.id;
+            var results = Meteor.users.find({_id: id}, {fields: {'profile': 1}}).fetch();
+            return results
+        }
+    }
+});
