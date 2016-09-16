e0ef487538e63c48799881c142c25adb548048e7
Nilson Ivano <nilson.ivano@gmail.com>
2016-09-16 08:34:47 -0300

    Consertando o flicker do lemeatHome

--- client/templates/lemeatSite/lemeatHome.js
+++ client/templates/lemeatSite/lemeatHome.js
@@ -1,15 +1,15 @@
-Template.lemeatHome.onRendered(function(){
+Template.lemeatHome.onRendered(function () {
     //Ativating Parallax
     $('.parallax').parallax();
     Session.set('userLocAccuracy', 100000);
 
     //Populating Map
-    GoogleMaps.ready('map', function(){
+    GoogleMaps.ready('map', function () {
         var currentTime = new Date();
-        var agendaFromNow = truckAgenda.find({dateEnd: {$gte :currentTime}}, {sort: {dateStart: 1}}).fetch();
+        var agendaFromNow = truckAgenda.find({dateEnd: {$gte: currentTime}}, {sort: {dateStart: 1}}).fetch();
         var map = GoogleMaps.maps.map.instance;
         var markerImage = '/images/Lemeat_marker_40.png';
-        placeMarkerTruckMap(agendaFromNow,map,markerImage);
+        placeMarkerTruckMap(agendaFromNow, map, markerImage);
         var locationMarker = "/images/location.png";
         var latNumber = Geolocation.latLng().lat;
         var lngNumber = Geolocation.latLng().lng;
@@ -23,108 +23,101 @@ Template.lemeatHome.onRendered(function(){
 });
 
 Template.lemeatHome.helpers({
-    mapOptions: function() {
+    mapOptions: function () {
         if (GoogleMaps.loaded() && Geolocation.latLng()) {
             var userLocation = Geolocation.latLng();
             var lat = userLocation.lat;
             var lng = userLocation.lng;
             return {
-                center: new google.maps.LatLng(lat,lng),
+                center: new google.maps.LatLng(lat, lng),
                 zoom: 14
             };
         }
     },
-    cardInfoOrderedByDistance: function(){
-        var userLoc = Geolocation.currentLocation();
-        var userLocAccuracy = userLoc.coords.accuracy;
-        var userLocAccuracyOld = Session.get('userLocAccuracy');
-        var userLocAccuracyDif = Math.abs(userLocAccuracy - userLocAccuracyOld);
-        console.log(userLocAccuracyDif);
-        if(userLoc && userLocAccuracyDif >= 10){
-            var agendaDistance = new Mongo.Collection(null);
-            var orderedResults = [];
-            var currentTime = new Date();
-            var userLocation = Geolocation.latLng();
-            var userLat = userLocation.lat;
-            var userLng = userLocation.lng;
-            var agendaList = truckAgenda.find({dateEnd: {$gte :currentTime}}, {sort: {dateStart: 1}}).fetch();
-            for(i=0; i <agendaList.length; i++){
-                var agendaLat = agendaList[i].lat;
-                var agendaLng = agendaList[i].lng;
-                var distance = getDistanceFromLatLonInKm(userLat, userLng, agendaLat, agendaLng);
-                agendaList[i].userDistance = distance;
-                var dateStart = new Date(agendaList[i].dateStart);
-                dateStart.setHours(0,0,0,0);
-                agendaList[i].day = dateStart;
-                agendaDistance.insert(agendaList[i]);
-            }
-            var orderedList = agendaDistance.find({}, {sort: {day: 1, userDistance:1}}).fetch();
-            for (i in orderedList){
-                var user = Meteor.users.find({_id: orderedList[i].addedBy},{field: {profile: 1}}).fetch();
-                var userId = user[0]._id;
-                var userIdInOrderedResults = orderedResults.filter(function ( obj ) {
-                    return obj.addedBy === userId;
-                })[0];
-                if(!userIdInOrderedResults){
-                    var profile = user[0].profile;
-                    if(profile.description.length > 0 && profile.img.length > 0){
-                        var orderedResultsItem = {};
-                        orderedResultsItem = profile;
-                        orderedResultsItem.userDistance = orderedList[i].userDistance;
-                        orderedResultsItem.dateStart = orderedList[i].dateStart;
-                        orderedResultsItem.dateEnd = orderedList[i].dateEnd;
-                        orderedResultsItem.address = orderedList[i].address;
-                        orderedResultsItem.addedBy = orderedList[i].addedBy;
-                        if(currentTime >= orderedResultsItem.dateStart && currentTime <= orderedResultsItem.dateEnd){
-                            orderedResultsItem.statusOpen = true
+    cardInfoOrderedByDistance: function () {
+        if (Geolocation.currentLocation()) {
+            var userLoc = Geolocation.currentLocation();
+            var userLocAccuracy = userLoc.coords.accuracy;
+            var userLocAccuracyOld = Session.get('userLocAccuracy');
+            var userLocAccuracyDif = Math.abs(userLocAccuracyOld - userLocAccuracy);
+            console.log(userLoc, userLocAccuracy, userLocAccuracyOld, userLocAccuracyDif);
+            Session.set('userLocAccuracy', userLocAccuracy);
+            if (userLocAccuracyDif > 100) {
+                var agendaDistance = new Mongo.Collection(null);
+                var orderedResults = [];
+                var currentTime = new Date();
+                var userLocation = Geolocation.latLng();
+                var userLat = userLocation.lat;
+                var userLng = userLocation.lng;
+                var agendaList = truckAgenda.find({dateEnd: {$gte: currentTime}}, {sort: {dateStart: 1}}).fetch();
+                for (i = 0; i < agendaList.length; i++) {
+                    var agendaLat = agendaList[i].lat;
+                    var agendaLng = agendaList[i].lng;
+                    var distance = getDistanceFromLatLonInKm(userLat, userLng, agendaLat, agendaLng);
+                    agendaList[i].userDistance = distance;
+                    var dateStart = new Date(agendaList[i].dateStart);
+                    dateStart.setHours(0, 0, 0, 0);
+                    agendaList[i].day = dateStart;
+                    agendaDistance.insert(agendaList[i]);
+                }
+                var orderedList = agendaDistance.find({}, {sort: {day: 1, userDistance: 1}}).fetch();
+                for (i in orderedList) {
+                    var user = Meteor.users.find({_id: orderedList[i].addedBy}, {field: {profile: 1}}).fetch();
+                    var userId = user[0]._id;
+                    var userIdInOrderedResults = orderedResults.filter(function (obj) {
+                        return obj.addedBy === userId;
+                    })[0];
+                    if (!userIdInOrderedResults) {
+                        var profile = user[0].profile;
+                        if (profile.description.length > 0 && profile.img.length > 0) {
+                            var orderedResultsItem = {};
+                            orderedResultsItem = profile;
+                            orderedResultsItem.userDistance = orderedList[i].userDistance;
+                            orderedResultsItem.dateStart = orderedList[i].dateStart;
+                            orderedResultsItem.dateEnd = orderedList[i].dateEnd;
+                            orderedResultsItem.address = orderedList[i].address;
+                            orderedResultsItem.addedBy = orderedList[i].addedBy;
+                            if (currentTime >= orderedResultsItem.dateStart && currentTime <= orderedResultsItem.dateEnd) {
+                                orderedResultsItem.statusOpen = true
                             } else {
                                 orderedResultsItem.statusOpen = false
                             }
-                        orderedResults.push(orderedResultsItem);
+                            orderedResults.push(orderedResultsItem);
                         }
                     }
                 }
-            var trucks = Meteor.users.find().fetch();
-            while(orderedResults.length < 9){
-                var randomTruck = Random.choice(trucks);
-                var randomTruckProfile = randomTruck.profile;
-                randomTruckProfile.addedBy = randomTruck._id;
-                var countFound = 0;
-                for(i in orderedResults){
-                    if(orderedResults[i].name == randomTruckProfile.name){
-                        countFound++;
+                var trucks = Meteor.users.find().fetch();
+                while (orderedResults.length < 9) {
+                    var randomTruck = Random.choice(trucks);
+                    var randomTruckProfile = randomTruck.profile;
+                    randomTruckProfile.addedBy = randomTruck._id;
+                    var countFound = 0;
+                    for (i in orderedResults) {
+                        if (orderedResults[i].name == randomTruckProfile.name) {
+                            countFound++;
+                        }
+                    }
+                    var truckDescription = randomTruckProfile.description;
+                    if (truckDescription) {
+                        var truckDescriptionLength = truckDescription.length;
+                        if (countFound == 0 && randomTruckProfile.img && truckDescriptionLength > 30) {
+                            orderedResults.push(randomTruckProfile);
+                        }
                     }
                 }
-                if(countFound == 0 && randomTruckProfile.img){
-                    orderedResults.push(randomTruckProfile);
-                }
+                Session.set('orderedResultsOld', orderedResults);
+                console.log('if' + orderedResults);
+                return orderedResults
+            } else{
+                var orderedResults = Session.get('orderedResultsOld');
+                console.log('else' + orderedResults);
+                return orderedResults
             }
-            Session.set('userLocAccuracy',userLocAccuracy);
-            return orderedResults
-        } else {
-            var trucks = Meteor.users.find().fetch();
-            var orderedResults = [];
-            while(orderedResults.length < 9){
-                var randomTruck = Random.choice(trucks);
-                var randomTruckProfile = randomTruck.profile;
-                randomTruckProfile.addedBy = randomTruck._id;
-                var countFound = 0;
-                for(i in orderedResults){
-                    if(orderedResults[i].name == randomTruckProfile.name){
-                        countFound++;
-                    }
-                }
-                if(countFound == 0 && randomTruckProfile.img){
-                    orderedResults.push(randomTruckProfile);
-                }
-            }
-            Session.set('userLocAccuracy',userLocAccuracy);
-            return orderedResults
         }
     }
 });
 
-Template.eventForm.onRendered(function(){
+Template.eventForm.onRendered(function () {
     this.autorun(function () {
         if (GoogleMaps.loaded()) {
             $("#eventAddress").geocomplete();
@@ -133,13 +126,13 @@ Template.eventForm.onRendered(function(){
 });
 
 Template.eventForm.helpers({
-    eventTypes: function(){
+    eventTypes: function () {
         return eventTypes
     }
 });
 
 Template.eventForm.events({
-    'click #eventSubmit': function(event){
+    'click #eventSubmit': function (event) {
         event.preventDefault();
         var name = $('[id=name]').val();
         var email = $('[id=email]').val();
@@ -153,26 +146,26 @@ Template.eventForm.events({
         var eventEnd = $('[id=eventDateEnd]').val();
         var eventDateStart = new Date(eventStart);
         var eventDateEnd = new Date(eventEnd);
-        if(Router.current().params.truckId){
+        if (Router.current().params.truckId) {
             var visibleToTruck = [Router.current().params.truckId];
             var visibleToAll = false
-        } else{
+        } else {
             var visibleToAll = true
         }
         var address = eventAddress + " " + eventCity;
         //Geocoding
         geocoder = new google.maps.Geocoder();
-        geocoder.geocode({'address': address}, function(results, status){
-            if(status == google.maps.GeocoderStatus.OK){
+        geocoder.geocode({'address': address}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
                 var eventAddress = results[0].formatted_address;
                 var eventCity = results[0].address_components[4].long_name;
                 var lat = results[0].geometry.location.lat();
                 var lng = results[0].geometry.location.lng();
-                Meteor.call('insertEvent',eventDateStart,eventDateEnd,eventAddress,eventCity, lat, lng, name, phone,
-                    email, eventType, eventDescription, visibleToAll, visibleToTruck, function(err){
-                        if(err){
+                Meteor.call('insertEvent', eventDateStart, eventDateEnd, eventAddress, eventCity, lat, lng, name, phone,
+                    email, eventType, eventDescription, visibleToAll, visibleToTruck, function (err) {
+                        if (err) {
                             Materialize.toast('Ops, ocorreu um erro no envio', 4000, 'red')
-                        } else{
+                        } else {
                             Materialize.toast('Enviada com sucesso. Você receberá um email com confirmação do envio', 4000, 'green');
                             var html =
                                 '<div>' + 'Olá ' + name + ', sua proposta foi enviada aos Food Trucks do Lemeat.' +
@@ -207,11 +200,11 @@ Template.eventForm.events({
             }
         })
     },
-    'blur #eventAddress': function(){
+    'blur #eventAddress': function () {
         var eventAddress = $('[id=eventAddress]').val();
         geocoder = new google.maps.Geocoder();
-        geocoder.geocode({'address': eventAddress}, function(results, status){
-            if(status == google.maps.GeocoderStatus.OK){
+        geocoder.geocode({'address': eventAddress}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
                 var city = results[0].address_components[4].long_name;
                 $('[id=eventCity]').val(city);
             }
@@ -220,10 +213,10 @@ Template.eventForm.events({
 });
 
 Template.truckTags.helpers({
-    allCities: function(){
+    allCities: function () {
         return ReactiveMethod.call("getAllCities");
     },
-    allTags: function(){
+    allTags: function () {
         return ReactiveMethod.call("getAllTags");
     }
 });
